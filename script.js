3ccd4e1e2dde92c9a25b2675800a172a3aab5e57
Rareș Mărincaș <rares.marincas@lucaciu.ro>
2025-12-20 18:36:49 +0200

    S

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("C").onmouseover = function(){
+    document.getElementById("C").src = "1.png";
+}
+document.getElementById("C").onmouseout = function(){
+    document.getElementById("C").src = "Bici.png";
+}
